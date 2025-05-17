56dafd46d1cd1d3282059f29d42df3e80123309d
AlanPratama <pralan76@gmail.com>
2025-05-17 21:08:17 +0700

    link social, aboust us

---
+++ src/utils/social.js
@@ -0,0 +1,26 @@
+export const linkSocial = {
+    whatsapp: "https://web.whatsapp.com/send?phone=6285716457103&text=Hallo!%20Ashdaq%20Paint",
+    instagram: "https://www.instagram.com/",
+    facebook: "https://www.facebook.com/",
+    twitter: "https://twitter.com/",
+    youtube: "https://www.youtube.com/",
+    tiktok: "https://www.tiktok.com/",
+    email: {
+        key: "admin@ashdaq.com",
+        value: "admin@ashdaq.com"
+    },
+    phone: {
+        key: "+62 857-1645-7103",
+        value: "https://api.whatsapp.com/send?phone=6285716457103&text=Hallo%20Admin%20Ashdaq%20Paint"
+    },
+    address: {
+        key: "PT. Berkat Bersama Paint. Agung Perkasa Street 8 K1, Kav. Numb. 41, RT.10/ RW. 11 | 14350 South Jakarta",
+        value: "https://maps.app.goo.gl/bQCgtcTfGHYdtBwo8"
+    },
+    shopee: "https://shopee.co.id/",
+    tokopedia: "https://www.tokopedia.com/",
+    ashdaq: {
+        key: "Ashdaq Paint",
+        value: "https://ashdaq.com/",
+    },
+}
