1487c9fae9ef3c1b801484384ae6d91ac677f1ec
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2025-05-18 16:03:08 +0700

    feat: update default modal to false

--- src/features/modal/zustand/useModalStore.js
+++ src/features/modal/zustand/useModalStore.js
@@ -1,27 +1,45 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 const useModalStore = create(
   (set) => ({
-    isShowModal: true,
-    modalType: "INFO",
-    modalVariant: "DEFAULT",
-    headMessage: "Toko Koko Lie",
-    message: "Halo Apa Kabar?",
+    isShowModal: false,
+    modalType: 'INFO',
+    modalVariant: 'DEFAULT',
+    headMessage: 'Toko Koko Lie',
+    message: 'Halo Apa Kabar?',
     onConfirm: () => {},
 
-    showModal: (modalType = "INFO", modalVariant = "DEFAULT", headMessage = "Toko Koko Lie", message = "Halo Apa Kabar?", onConfirm = () => {}) => {
-      set({ isShowModal: true, modalType, modalVariant, headMessage, message, onConfirm });
-    },
-    
-    hideModal: () => {
-      set({ isShowModal: false, modalType: "INFO", modalVariant: "DEFAULT", headMessage: "Toko Koko Lie", message: "Halo Apa Kabar?", onConfirm: () => {} });
+    showModal: (
+      modalType = 'INFO',
+      modalVariant = 'DEFAULT',
+      headMessage = 'Toko Koko Lie',
+      message = 'Halo Apa Kabar?',
+      onConfirm = () => {}
+    ) => {
+      set({
+        isShowModal: true,
+        modalType,
+        modalVariant,
+        headMessage,
+        message,
+        onConfirm
+      });
     },
 
+    hideModal: () => {
+      set({
+        isShowModal: false,
+        modalType: 'INFO',
+        modalVariant: 'DEFAULT',
+        headMessage: 'Toko Koko Lie',
+        message: 'Halo Apa Kabar?',
+        onConfirm: () => {}
+      });
+    }
   }),
   {
-    name: "modal-storage",
+    name: 'modal-storage'
   }
 );
 
 export default useModalStore;
-
